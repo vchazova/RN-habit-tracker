56e69bb0b960bb23f68235354611de4fdee8d7c7
Valerka.ya <lera.chazova2012@yandex.ru>
2025-08-07 22:34:01 +0300

    create auth page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+function RouteGuard({ children }: { children: React.ReactNode }) {
+
+  const isAuth = false;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuth) {
+      // router.replace('/auth')
+    }
+  });
+
+  return <>{children}</>
+}
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>;
+  return <RouteGuard>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  </RouteGuard>;
 }
