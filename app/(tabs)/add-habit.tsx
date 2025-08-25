666c39c6f1170a6a661da42d209f0a0a15d55986
Valerka.ya <lera.chazova2012@yandex.ru>
2025-08-25 18:03:09 +0300

    add form field for new habit

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
+
+const FREQUENCES = ["daily", "weekly", "monthly"];
 
 export default function AddHabitScreen() {
   return (
     <View>
-      {""}
-      <Text>AddHabitScreen</Text>
+      <TextInput label="Title" mode="outlined" />
+      <TextInput label="Description" mode="outlined" />
+      <View>
+        <SegmentedButtons
+          buttons={FREQUENCES.map((frequency) => ({
+            value: frequency,
+            label: frequency.charAt(0).toUpperCase() + frequency.slice(1),
+          }))}
+        />
+      </View>
+      <Button mode="contained">Add Habit</Button>
     </View>
   );
 }
